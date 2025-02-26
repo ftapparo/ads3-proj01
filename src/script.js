d63debf4221bdba5e4f5e7bf225bafa92d6ccead
FLAVIOTAPPAAB6C\flaviotapparo <tapparo.flavio@gmail.com>
2025-02-26 20:06:14 -0300

    Meu primeiro commit

---
+++ src/script.js
@@ -0,0 +1,10 @@
+function calcularIdade(){
+    let anoNascimento = document.getElementById("anoNascimento").value;
+    let idade = 2025 - anoNascimento;
+    document.getElementById("resultado").innerHTML = `Idade é : ${idade}`;
+}
+
+// Novas implementações
+// - Utilizar o ano atual.
+// - Aceitar apenas ano maior do que 1900.
+// - Não permitir que o input seja vazio.
